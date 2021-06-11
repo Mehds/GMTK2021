0ff04db9bbd9cd1d51663aab4d0b202f871a253f
Mehds <oulmakki@gmail.com>
2021-06-11 23:17:25 +0100

    initial commit
    Initial commit

---
+++ Assets/Scripts/Berzerker.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Berzerker : MonoBehaviour
+{   
+    public Transform transform = null;
+    public float movementSpeed = 0.1f;
+
+    public float minimumDistance = 0.5f;
+
+    public GameObject target = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 direction = target.transform.position - transform.position;
+        
+        if (direction.magnitude > minimumDistance) {
+         transform.position = transform.position + Vector3.Scale(Vector3.Normalize(direction),new Vector3(movementSpeed, movementSpeed, 0) );
+        }
+
+        // To do
+
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 0.3f;
+    public int health = 1;
+    public Rigidbody2D rb2d = null;
+    public Transform transform = null;
+
+    public GameObject anchor = null;
+    public LineRenderer lineRenderer = null;
+    
+    public float maxDistance = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if (GetComponent<Rigidbody2d>()){
+        //     rb2d = GetComponent<Rigidbody2d>();
+        // }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w")){
+            handleMovement( new Vector3(0, movementSpeed, 0));
+        }
+
+        if (Input.GetKey("s")){
+            handleMovement( new Vector3(0, -1 * movementSpeed, 0));
+
+            // transform.position = transform.position + new Vector3(0, -1 * movementSpeed, 0) ;
+        }
+
+        if (Input.GetKey("a")){
+            handleMovement( new Vector3(-1 * movementSpeed, 0, 0));
+
+            // transform.position = transform.position + new Vector3(-1 * movementSpeed, 0, 0) ;
+        }
+
+        if (Input.GetKey("d")){
+            handleMovement( new Vector3(movementSpeed, 0, 0));
+
+            // transform.position = transform.position + new Vector3(movementSpeed, 0, 0) ;
+        }
+
+        lineRenderer.startWidth = 0.3f;
+        lineRenderer.endWidth = 0.3f;
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, anchor.transform.position);
+
+    }
+
+    void handleMovement(Vector3 intendedMovement){
+        
+        Vector3 newPosition = transform.position + intendedMovement;
+        float distance = (newPosition - anchor.transform.position).magnitude;
+
+        if (distance < maxDistance){
+            transform.position = newPosition;
+        }
+
+        // TO-DO
+        // Else, get yanked
+
+        // Idea: Movement speed is a function of distance?
+
+    }
+}
