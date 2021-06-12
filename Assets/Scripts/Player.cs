38630dd727a4d2d6ccae23f7ef7a2482c182da05
NorthOrion <39065667+NorthOrion@users.noreply.github.com>
2021-06-12 14:27:18 +0200

    Dodge mechanic : basics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float movementSpeed = 0.3f;
+
+    public float dodgeSpeed = 0.6f;
+    public float dodgeMultiplier = 2;
+    public int dodgeTimerFPS = 15; 
     public int health = 1;
     public Rigidbody2D rb2d = null;
     public Transform transform = null;
@@ -14,38 +18,67 @@ public class Player : MonoBehaviour
     
     public float maxDistance = 5f;
 
+    //Private params
+
+    private bool isPressedUp = false;
+    private bool isPressedDown = false;
+    private bool isPressedLeft = false;
+    private bool isPressedRight = false;
+    private bool isPressedDodge = false;
+
+    private int dodgeTimerCounter = 0;
+    private bool isDodging = false;
+    private bool isImmune = false;
+    private SpriteRenderer characterSprite = null;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        // if (GetComponent<Rigidbody2d>()){
-        //     rb2d = GetComponent<Rigidbody2d>();
-        // }
+        characterSprite = GetComponent<SpriteRenderer>();
+        characterSprite.color = Color.white;
     }
 
     // Update is called once per frame
+
+    void Update()
+    {
+        GetPlayerInput();
+        SetPlayerState();
+        SetPlayerEffects();
+    }
     void FixedUpdate()
     {
-        if (Input.GetKey("w") || Input.GetKey("up")){
-            handleMovement( new Vector3(0, movementSpeed, 0));
+        Vector3 movementDirection = new Vector3(0,0,0);
+        if (isPressedUp){
+            movementDirection = movementDirection + new Vector3(0, movementSpeed, 0);
+            isPressedUp = false;
         }
 
-        if (Input.GetKey("s") || Input.GetKey("down")){
-            handleMovement( new Vector3(0, -1 * movementSpeed, 0));
-
-            // transform.position = transform.position + new Vector3(0, -1 * movementSpeed, 0) ;
+        if (isPressedDown){
+            movementDirection = movementDirection + new Vector3(0, -1 * movementSpeed, 0);
+            isPressedDown = false;
         }
 
-        if (Input.GetKey("a") || Input.GetKey("left")){
-            handleMovement( new Vector3(-1 * movementSpeed, 0, 0));
-
-            // transform.position = transform.position + new Vector3(-1 * movementSpeed, 0, 0) ;
+        if (isPressedLeft){
+            movementDirection = movementDirection + new Vector3(-1 * movementSpeed, 0, 0);
+            isPressedLeft = false;
         }
 
-        if (Input.GetKey("d") || Input.GetKey("right")){
-            handleMovement( new Vector3(movementSpeed, 0, 0));
-
-            // transform.position = transform.position + new Vector3(movementSpeed, 0, 0) ;
+        if (isPressedRight){
+            movementDirection = movementDirection + new Vector3(movementSpeed, 0, 0);
+            isPressedRight = false;
         }
+
+        if(isPressedDodge)
+        {
+            isDodging = true;
+            dodgeTimerCounter = dodgeTimerFPS;
+            isPressedDodge = false;
+        }
+
+        handleMovement(movementDirection);
+
 
         lineRenderer.startWidth = 0.3f;
         lineRenderer.endWidth = 0.3f;
@@ -56,6 +89,14 @@ public class Player : MonoBehaviour
     }
 
     void handleMovement(Vector3 intendedMovement){
+        
+        
+
+        if(isDodging)
+        {
+            intendedMovement = Vector3.Scale (intendedMovement, new Vector3(dodgeMultiplier, dodgeMultiplier, 0));
+        }
+                
         
         Vector3 newPosition = transform.position + intendedMovement;
         float distance = (newPosition - anchor.transform.position).magnitude;
@@ -70,4 +111,80 @@ public class Player : MonoBehaviour
         // Idea: Movement speed is a function of distance?
 
     }
+
+    void GetPlayerInput()
+    {
+        if (Input.GetKey("w") || Input.GetKey("up")){
+            isPressedUp = true;
+        }
+
+        if (Input.GetKey("s") || Input.GetKey("down")){
+            isPressedDown = true;
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left")){
+            isPressedLeft = true;
+        }
+
+        if (Input.GetKey("d") || Input.GetKey("right")){
+            isPressedRight = true;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            isPressedDodge = true;
+        }
+    }
+
+    void SetPlayerState()
+    {
+        //Dodge state
+        if (dodgeTimerCounter != 0)
+        {
+            dodgeTimerCounter = dodgeTimerCounter - 1;
+        }
+        else
+        {
+            isDodging = false;
+        }
+
+        //Immune state
+        isImmune = isDodging;
+    }
+
+    Vector3 GetDodgeMovement()
+    {
+        Vector3 dodgeMovement = new Vector3(0,0,0);
+        if (isPressedUp){
+            dodgeMovement = dodgeMovement + new Vector3(0, dodgeSpeed, 0);
+        }
+
+        if (isPressedDown){
+            dodgeMovement = dodgeMovement + new Vector3(0, -1 * dodgeSpeed, 0);
+        }
+
+        if (isPressedLeft){
+            dodgeMovement = dodgeMovement + new Vector3(-1 * dodgeSpeed, 0, 0);
+        }
+
+        if (isPressedRight){
+            dodgeMovement = dodgeMovement + new Vector3(dodgeSpeed, 0, 0);
+        }
+
+        return dodgeMovement;
+    }
+
+    void SetPlayerEffects()
+    {
+        if (isImmune)
+        {
+            characterSprite.color = Color.cyan;
+        }
+        else
+        {
+            characterSprite.color = Color.white;
+        }
+    }
+
+
 }
